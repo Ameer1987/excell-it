1cab6c62e93dbca814000ee1c379c45a3debf26b
Ameer Sameer <ext.ameer@gmail.com>
2017-03-08 11:15:46 +0200

    add homepage controller

---
+++ app/Controller/HomepageController.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @brief Classrooms controller.
+ */
+class HomepageController extends AppController {
+
+    var $name = 'Homepage';
+
+    function beforeFilter() {
+        
+    }
+
+    /**
+     */
+    function index() {
+        $this->set('sections', 'dsfsdf');
+    }
+
+}
