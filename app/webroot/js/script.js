c2dc2cfdc1c5ca4fc9652830f0959b9c0494cbbd
Ameer Sameer <ext.ameer@gmail.com>
2017-03-09 22:35:15 +0200

    factorizing  homepage template

--- app/webroot/js/script.js
+++ app/webroot/js/script.js
@@ -1,5 +1,5 @@
 function include(scriptUrl) {
-    document.write('<script src="' + scriptUrl + '"></script>');
+    document.write('<script src="/excell-it/' + scriptUrl + '"></script>');
 }
 
 function isIE() {
