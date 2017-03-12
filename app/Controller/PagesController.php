2ab2e5e3be5abfe328f534400309f672259e06af
Ameer Sameer <ext.ameer@gmail.com>
2017-03-12 14:56:46 +0200

    add services and service snippets

--- app/Controller/PagesController.php
+++ app/Controller/PagesController.php
@@ -24,6 +24,14 @@ class PagesController extends AppController {
      */
     function services() {
         $this->layout = 'pages';
+
+        $this->loadModel('Service');
+        $services = $this->Service->find('all');
+        $this->set('services', $services);
+
+        $this->loadModel('ServiceSnippet');
+        $services_snippets = $this->ServiceSnippet->find('all');
+        $this->set('services_snippet', $services_snippets);
     }
 
     /**
@@ -50,6 +58,14 @@ class PagesController extends AppController {
         $this->loadModel('Career');
         $career = $this->Career->findById($career_id);
         $this->set('career', $career);
+    }
+
+    function displayService($service_id) {
+        $this->layout = 'pages';
+
+        $this->loadModel('Service');
+        $service = $this->Service->findById($service_id);
+        $this->set('service', $service);
     }
 
 }
