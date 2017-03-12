874005fe58f0a76ea7c88b8dc8fe8ef1ff50f4c3
Ameer Sameer <ext.ameer@gmail.com>
2017-03-12 19:04:46 +0200

    service_snippet  fix name

--- app/Model/ServiceSnippet.php
+++ app/Model/ServiceSnippet.php
@@ -3,7 +3,32 @@ App::uses('AppModel', 'Model');
 /**
  * ServiceSnippet Model
  *
+ * @property ServiceSnippetPoint $ServiceSnippetPoint
  */
 class ServiceSnippet extends AppModel {
+
+
+	// The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * hasMany associations
+ *
+ * @var array
+ */
+	public $hasMany = array(
+		'ServiceSnippetPoint' => array(
+			'className' => 'ServiceSnippetPoint',
+			'foreignKey' => 'service_snippet_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)
+	);
 
 }
