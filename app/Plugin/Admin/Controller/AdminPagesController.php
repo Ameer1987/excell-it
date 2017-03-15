d885dd50e933b72558f23069cfa493e4532f76e9
Magdy Yackoub <magdy.yackoub@mts-egypt.com>
2017-03-15 21:46:08 +0200

    Solve Admin edit

--- app/Plugin/Admin/Controller/AdminPagesController.php
+++ app/Plugin/Admin/Controller/AdminPagesController.php
@@ -1,5 +1,7 @@
 <?php
+
 App::uses('AdminAppController', 'Admin.Controller');
+
 /**
  * AdminPagesController
  *
@@ -7,33 +9,33 @@ App::uses('AdminAppController', 'Admin.Controller');
  */
 class AdminPagesController extends AdminAppController {
 
-	public $uses = array('Page');
+    public $uses = array('Page');
 
     public function index() {
-	    $conditions = array();
+        $conditions = array();
         $pagesTableURL = array('controller' => 'admin_pages', 'action' => 'index');
 
         //join get query & named params
         $params = array_merge($this->request->params['named']);
-        foreach($this->request->query as $key => $value) $params[$key] = $value;
+        foreach ($this->request->query as $key => $value)
+            $params[$key] = $value;
 
-        foreach($params as $key => $value) {
+        foreach ($params as $key => $value) {
             $split = explode('-', $key);
             $modelName = (sizeof($split) > 1) ? $split[0] : 'Page';
             $property = (sizeof($split) > 1) ? $split[1] : $key;
-            if($modelName == 'Page' || !empty($this->Page->belongsTo[$modelName])) {
+            if ($modelName == 'Page' || !empty($this->Page->belongsTo[$modelName])) {
                 $this->loadModel($modelName);
                 $modelObj = new $modelName();
-                if(!empty($modelObj)) {
+                if (!empty($modelObj)) {
                     $columnType = $modelObj->getColumnType($property);
-                    if(!empty($columnType)){
+                    if (!empty($columnType)) {
                         //add it to url
                         $pagesTableURL[$key] = $value;
                         //add it to conditions
-                        switch($columnType)
-                        {
+                        switch ($columnType) {
                             case 'string':
-                                $conditions[$modelName . '.' . $property . ' LIKE'] = '%'.$value.'%';
+                                $conditions[$modelName . '.' . $property . ' LIKE'] = '%' . $value . '%';
                                 break;
                             default:
                                 $conditions[$modelName . '.' . $property] = $value;
@@ -42,21 +44,19 @@ class AdminPagesController extends AdminAppController {
                     }
                 }
             }
-
         }
 
-		$this->Page->recursive = 0;
-		$this->paginate = array('conditions' => $conditions, 'limit' => 15);
+        $this->Page->recursive = 0;
+        $this->paginate = array('conditions' => $conditions, 'limit' => 15);
         $this->set('pages', $this->Paginator->paginate('Page'));
-		$this->set('pagesTableURL', $pagesTableURL);
+        $this->set('pagesTableURL', $pagesTableURL);
         $this->set('pagesTableModelAlias', 'Page');
-		//render as local table if it is an ajax request
-        if($this->request->is('ajax'))
-        {
+        //render as local table if it is an ajax request
+        if ($this->request->is('ajax')) {
             $this->render('table');
         }
-	}
-    
+    }
+
     public function view($id = null) {
         $this->Page->id = $id;
         if (!$this->Page->exists()) {
@@ -65,106 +65,105 @@ class AdminPagesController extends AdminAppController {
         $page = $this->Page->read(null, $id);
         $this->set('page', $page);
     }
-    
-	public function add() {
-		if ($this->request->is('post')) {
-			$this->Page->create();
-			if ($this->Page->save($this->request->data)) {
-				$this->Session->setFlash(__('The page has been saved'), 'default', array(), 'good');
-                if(!empty($this->request->query['redirect'])) {
-					$this->redirect($this->redirectUrl);
-				} else {
-					$this->redirect(array('action' => 'view', $this->Page->id));
-				}
-			} else {
-				$this->Session->setFlash(__('The page could not be saved. Please, try again.'), 'default', array(), 'bad');
-			}
-		} else {
+
+    public function add() {
+        if ($this->request->is('post')) {
+            $this->Page->create();
+            if ($this->Page->save($this->request->data)) {
+                $this->Session->setFlash(__('The page has been saved'), 'default', array(), 'good');
+                if (!empty($this->request->query['redirect'])) {
+                    $this->redirect($this->redirectUrl);
+                } else {
+                    $this->redirect(array('action' => 'view', $this->Page->id));
+                }
+            } else {
+                $this->Session->setFlash(__('The page could not be saved. Please, try again.'), 'default', array(), 'bad');
+            }
+        } else {
             //add the named params as data
-            foreach($this->request->params['named'] as $param => $value) {
+            foreach ($this->request->params['named'] as $param => $value) {
                 $columnType = $this->Page->getColumnType($param);
-                if(!empty($columnType)) {
-                    if(empty($this->request->data['Page'])) $this->request->data['Page'] = array();
+                if (!empty($columnType)) {
+                    if (empty($this->request->data['Page']))
+                        $this->request->data['Page'] = array();
                     $this->request->data['Page'][$param] = $value;
                     //is this a reference to a related object?
                     foreach ($this->Page->belongsTo as $relationName => $relationInfo) {
-                    	if($relationInfo['foreignKey'] == $param) {
-                    		$relatedRecord = $this->Page->$relationInfo['className']->find('first', array('conditions' => array($relationInfo['className'] . '.id' => $value), 'recursive' => 0));
-                    		$this->set(Inflector::variable($relationInfo['className']), $relatedRecord);
-                    	}
+                        if ($relationInfo['foreignKey'] == $param) {
+                            $relatedRecord = $this->Page->$relationInfo['className']->find('first', array('conditions' => array($relationInfo['className'] . '.id' => $value), 'recursive' => 0));
+                            $this->set(Inflector::variable($relationInfo['className']), $relatedRecord);
+                        }
                     }
                 }
             }
         }
-	}
+    }
 
+    public function edit($id = null) {
+        $this->Page->id = $id;
+        if (!$this->Page->exists()) {
+            throw new NotFoundException(__('Invalid page'));
+        }
+        if ($this->request->is('post') || $this->request->is('put')) {
+            if ($this->Page->save($this->request->data)) {
+                $this->Session->setFlash(__('The page has been saved'), 'default', array(), 'good');
+                $this->redirect($this->redirectUrl);
+            } else {
+                $this->Session->setFlash(__('The page could not be saved. Please, try again.'), 'default', array(), 'bad');
+            }
+        } else {
+            $page = $this->Page->read(null, $id);
+            $this->request->data = $page;
+            $this->set('page', $page);
+        }
+    }
 
-	public function edit($id = null) {
-		$this->Page->id = $id;
-		if (!$this->Page->exists()) {
-			throw new NotFoundException(__('Invalid page'));
-		}
-		if ($this->request->is('post') || $this->request->is('put')) {
-			if ($this->Page->save($this->request->data)) {
-				$this->Session->setFlash(__('The page has been saved'), 'default', array(), 'good');
-				$this->redirect($this->redirectUrl);
-			} else {
-				$this->Session->setFlash(__('The page could not be saved. Please, try again.'), 'default', array(), 'bad');
-			}
-		} else {
-			$page = $this->Page->read(null, $id);
-			$this->request->data = $page;
-			$this->set('page', $page);
-		}
-	}
-
-
-	public function delete($id = null) {
-		if (!$this->request->is('post')) {
-			throw new MethodNotAllowedException();
-		}
-		$this->Page->id = $id;
-		if (!$this->Page->exists()) {
-			throw new NotFoundException(__('Invalid page'));
-		}
-		if ($this->Page->delete()) {
-			$this->Session->setFlash(__('Page deleted'), 'default', array(), 'good');
+    public function delete($id = null) {
+        if (!$this->request->is('post')) {
+            throw new MethodNotAllowedException();
+        }
+        $this->Page->id = $id;
+        if (!$this->Page->exists()) {
+            throw new NotFoundException(__('Invalid page'));
+        }
+        if ($this->Page->delete()) {
+            $this->Session->setFlash(__('Page deleted'), 'default', array(), 'good');
             $this->redirect($this->redirectUrl);
-		}
-		$this->Session->setFlash(__('Page was not deleted'), 'default', array(), 'bad');
-		$this->redirect(array('action' => 'index'));
-	}
-        
-        	public function display() {
-		$path = func_get_args();
+        }
+        $this->Session->setFlash(__('Page was not deleted'), 'default', array(), 'bad');
+        $this->redirect(array('action' => 'index'));
+    }
 
-		$count = count($path);
-		if (!$count) {
-			$this->redirect('/');
-		}
-		$page = $subpage = $title_for_layout = null;
+    public function display() {
+        $path = func_get_args();
 
-		if (!empty($path[0])) {
-			$page = $path[0];
-		}
-		if (!empty($path[1])) {
-			$subpage = $path[1];
-		}
-		if (!empty($path[$count - 1])) {
-			$title_for_layout = Inflector::humanize($path[$count - 1]);
-		}
-		$this->set(compact('page', 'subpage', 'title_for_layout'));
-		$this->render(implode('/', $path));
-	}
+        $count = count($path);
+        if (!$count) {
+            $this->redirect('/');
+        }
+        $page = $subpage = $title_for_layout = null;
 
-	public function auth_redirect_fix() {
-		$url = $this->request->here;
-		$pos = strpos($url, '/' . basename(ROOT));
-		while($pos === 0) {
-			$url = substr($url, strlen('/' . basename(ROOT)));
-			$pos = strpos($url, '/' . basename(ROOT));
-		}
-		$this->redirect($url);
-	}
+        if (!empty($path[0])) {
+            $page = $path[0];
+        }
+        if (!empty($path[1])) {
+            $subpage = $path[1];
+        }
+        if (!empty($path[$count - 1])) {
+            $title_for_layout = Inflector::humanize($path[$count - 1]);
+        }
+        $this->set(compact('page', 'subpage', 'title_for_layout'));
+        $this->render(implode('/', $path));
+    }
+
+    public function auth_redirect_fix() {
+        $url = $this->request->here;
+        $pos = strpos($url, '/' . basename(ROOT));
+        while ($pos === 0) {
+            $url = substr($url, strlen('/' . basename(ROOT)));
+            $pos = strpos($url, '/' . basename(ROOT));
+        }
+        $this->redirect($url);
+    }
 
 }
