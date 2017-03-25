26a26c2f6179e7a35aa1b1dabd406b16337c3c7c
Ameer Sameer <ext.ameer@gmail.com>
2017-03-25 12:48:14 +0200

    new contacts front

---
+++ app/Model/ContactGeneral.php
@@ -0,0 +1,23 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * ContactGeneral Model
+ *
+ */
+class ContactGeneral extends AppModel {
+
+/**
+ * Use table
+ *
+ * @var mixed False or table name
+ */
+	public $useTable = 'contact_general';
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'name';
+
+}
