3bb8ccee9571c0e30906456f20703ac01bc5fc63
Ameer Sameer <ext.ameer@gmail.com>
2017-03-25 12:48:06 +0200

    new contacts front

---
+++ app/Model/ContactBranch.php
@@ -0,0 +1,16 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * ContactBranch Model
+ *
+ */
+class ContactBranch extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'name';
+
+}
